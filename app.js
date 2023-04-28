06c6ed13371f29263394542aef0b748ac52697ae
manimanitin <andreman1781@gmail.com>
2023-04-28 09:28:19 -0600

    Manejo de rutas

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hola Mundo!');
+    res.send('Una API básica desde Express');
 });
+
+
+app.get('/saludo', function (req, res) {
+    res.send('Hola desde la API');
+});
+
+app.get('/despedida', function (req, res) {
+    res.send('Adios desde la API');
+});
+
 
 app.listen(3000, function () {
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
