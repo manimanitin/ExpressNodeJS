e790ecbc048ad4302fdad7a18805b41ff13634cd
manimanitin <andreman1781@gmail.com>
2023-05-08 09:42:21 -0600

    Metodos de usuarios

--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,40 @@ const router = (app) => {
             response.send(result);
         });
     });
+
+    app.get('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.post('/users', (request, response) => {
+        console.log(request.body);
+        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+            if (error) throw error;
+            response.status(201).send(`User added with id ${result.insertId}`);
+        });
+    });
+
+    app.put('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+            response.send('User updated successfully');
+        });
+    });
+
+    app.delete('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('DELETE users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted');
+        });
+    });
+
+
 };
 
 module.exports = router;
