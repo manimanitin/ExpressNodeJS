66f066ffca9f47f26fad6e136011343e77a17c36
manimanitin <andreman1781@gmail.com>
2023-05-09 09:24:25 -0600

    Rutas sin adaptar

--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,97 @@ const router = (app) => {
             message: 'Bienvenido a Node.js Express REST API'
         });
     });
+
+
+    //Users routes
+
     app.get('/users', async (request, response) => {
         const db = await pool;
-        const result = await db.request().query('SELECT * FROM users', (error, result) => {
+        const results = await db.request().query('SELECT * FROM users ', (error, result) => {
             if (error) throw error;
-            response.send(result.recordset);
+            const resultar = result.recordsets.map(resultado => ({ [resultado.key]: resultado.value }));
+            response.send(resultar.recordsets);
+        });
+    });
+
+    app.get('/users/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result.recordsets);
+        });
+    });
+
+    app.post('/users', async (request, response) => {
+        const db = await pool;
+        const results = await db.request().query('INSERT INTO users SET ?', request.body, (error, result) => {
+            if (error) throw error;
+            response.status(201).send(`User added with id ${result.insertId}`);
+        });
+    });
+
+    app.put('/users/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+            response.send('User updated successfully');
+        });
+    });
+
+    app.delete('/users/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted');
+        });
+    });
+
+    //Product routes
+
+
+    app.get('/products', async (request, response) => {
+        const db = await pool;
+        const results = await db.request().query('SELECT * FROM products', (error, result) => {
+            if (error) throw error;
+            response.send(result.recordsets);
+        });
+    });
+
+    app.get('/products/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('SELECT * FROM products WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result.recordsets);
+        });
+    });
+
+    app.post('/products', async (request, response) => {
+        const db = await pool;
+        const results = await db.request().query('INSERT INTO products SET ?', request.body, (error, result) => {
+            if (error) throw error;
+            response.status(201).send(`User added with id ${result.insertId}`);
+        });
+    });
+
+    app.put('/products/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('UPDATE products SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+            response.send('User updated successfully');
+        });
+    });
+
+    app.delete('/products/:id', async (request, response) => {
+        const db = await pool;
+        const id = request.params.id;
+        const results = await db.request().query('DELETE FROM products WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted');
         });
     });
 };
